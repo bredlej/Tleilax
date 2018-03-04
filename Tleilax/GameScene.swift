2a23a5d0d4d91f47fa5d080ccf2444371585f1fa
Patryk Szczypień <bredlej@mbp-patryk.home>
2018-03-04 16:45:55 +0100

    Initial Commit

---
+++ Tleilax/GameScene.swift
@@ -0,0 +1,77 @@
+//
+//  GameScene.swift
+//  Tleilax
+//
+//  Created by Patryk Szczypień on 04.03.2018.
+//  Copyright © 2018 Patryk Szczypień. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    private var playerNode : PlayerNode?
+    
+    override func sceneDidLoad() {
+
+        self.lastUpdateTime = 0
+        self.playerNode = self.childNode(withName: "Player") as? PlayerNode
+        self.playerNode?.initialSetup()
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        self.playerNode?.touchDown(atPoint: pos)
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        self.playerNode?.touchMoved(toPoint: pos)
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        self.playerNode?.touchUp(atPoint: pos)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+
+        
+        self.lastUpdateTime = currentTime
+    }
+}
